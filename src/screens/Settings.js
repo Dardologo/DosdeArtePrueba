34193229b23ece1dc4b06ae423b849f3e2e45c16
Dardologo <dardo_015@hotmail.com>
2022-12-04 21:33:54 -0300

    AsyncStorage

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,26 +1,42 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, Text } from 'react';
 import { Button, View, SafeAreaView} from 'react-native';
+import AuthContext from '../services/AuthContext';
+import { Avatar, Divider } from '@rneui/themed';
 import { StyleSheet } from 'react-native';
-import { AuthContext, authData } from '../services/AuthContext/index';
+
+
 
 
 
  const Settings =({ navigation, route }) => {
   //Queremos mostrar la info del usuario en settings y ya tenemos el objeto authenticationData
- // const {authenticationData} = useContext(AuthContext)
+ const {authenticationData, setauthenticationData} = useContext(AuthContext)
 
  console.log("Hola")
 
- // useEffect(() => {
-  // console.log(authenticationData);
-//}, [])
-    return (
+  useEffect(() => {
+   console.log(authenticationData);
+}, [])
+  const logout = () => {
+    setauthenticationData(null)
+  }
+  /*  return (
         
         <View style={styles.container}>
+           <Avatar
+              size={64}
+              rounded
+              source={{uri : authenticationData.picture}}
+            />
+            <Divider />
                < Button
                title='Logout'/>
+               onPress= {logout}
         </View>
-    )
+    )*/
+    return (<View>
+     
+    </View>)
 }
 const styles =StyleSheet.create({
     container:
