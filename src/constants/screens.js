bd69eb3c2d789f1530a0b9c8a348a816d45ba8ff
Dardologo <dardo_015@hotmail.com>
2022-12-11 15:23:05 -0300

    Constants

---
+++ src/constants/screens.js
@@ -0,0 +1,16 @@
+//
+const ROUTE_HOME = "Dos de Arte";
+const ROUTE_FAVOURITES_LAMPS = "Lámparas Favoritas";
+const ROUTE_SETTINGS = "Configuración";
+const ROUTE_LAMP_DETAIL = "Detalle de Lámpara";
+const ROUTE_MAP = "Sucursales";
+const ROUTE_LAMP_CATALOG = "Catálogo";
+
+export {
+  ROUTE_HOME,
+  ROUTE_FAVOURITES_LAMPS,
+  ROUTE_SETTINGS,
+  ROUTE_LAMP_DETAIL,
+  ROUTE_MAP,
+  ROUTE_LAMP_CATALOG,
+};
